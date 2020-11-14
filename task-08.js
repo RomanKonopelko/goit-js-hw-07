72f6ebdd4458184e091c47d6a4d3d4f86e63f1e0
RomanKonopelko <68011362+RomanKonopelko@users.noreply.github.com>
2020-11-14 21:31:45 +0200

    extra task-8

---
+++ task-08.js
@@ -0,0 +1,27 @@
+const quantity = document.querySelector('[type="number"]')
+const btnCreate = document.querySelector('[data-action="render"]')
+const btnDelete = document.querySelector('[data-action="destroy"]')
+const boxes = document.getElementById('boxes')
+
+function create(amount) { 
+    let num = 0;
+    let px = 10;
+    for (let i = 1; i <= amount; i += 1) {
+        num += 1
+        px += 10;
+        let div = document.createElement('div')
+        div.textContent = num
+        div.style.width = px + 'px'
+        div.style.height = px + 'px'
+        div.style.backgroundColor = `rgb( ${Math.random() * 256} ,  ${Math.random() * 256} ,  ${Math.random() * 256})`
+        boxes.append(div)
+     }
+}
+function render() {
+    boxes.innerHTML = '';
+    create(quantity.value)
+}
+btnCreate.addEventListener('click', render)
+btnDelete.addEventListener('click', () => { 
+    boxes.innerHTML = "";
+})
