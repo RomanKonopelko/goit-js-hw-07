5c09229354481886bd0fc7eda0cc0c872f211c26
RomanKonopelko <68011362+RomanKonopelko@users.noreply.github.com>
2020-11-15 13:55:40 +0200

    task-05 fixed

--- task-05.js
+++ task-05.js
@@ -2,8 +2,8 @@ const inputRef = document.getElementById('name-input')
 const outputRef = document.getElementById('name-output')
 
 inputRef.addEventListener('input', event => { 
-    event.target.value.length === 0 ?
-        outputRef.textContent = 'незнакомец' :
-        outputRef.textContent = event.target.value;
+      outputRef.textContent = event.target.value.length === 0 ?
+     'незнакомец' :
+      event.target.value;
     
 })
